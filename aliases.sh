1559375f24abff5b43134727364174bcb9682557
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-11-13 15:26:22 +0100

    added vim-vimrc alias

--- aliases.sh
+++ aliases.sh
@@ -64,6 +64,7 @@ set_alias_to_script_file () { _ $@
 #                            body                           #   
 set_alias v vim
 set_alias vc 'vim ~/.bashrc'
+set_alias vim-vimrc 'vim ~/.vimrc'
 
 set_alias ssh-server 'ssh nikiforo@ssh.mpi-muelheim.mpg.de'
 
